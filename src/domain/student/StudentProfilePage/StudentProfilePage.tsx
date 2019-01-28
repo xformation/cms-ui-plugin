03919c93c2baecf9f16710c7fd26cd1e954db43c
mustafa-synectiks <35029729+mustafa-synectiks@users.noreply.github.com>
2019-01-28 18:34:36 +0530

    student profile

--- src/domain/student/StudentProfilePage/StudentProfilePage.tsx
+++ src/domain/student/StudentProfilePage/StudentProfilePage.tsx
@@ -1,20 +1,20 @@
 import * as React from 'react';
 
-import { withRouter, RouteComponentProps, Link } from 'react-router-dom';
-import { graphql, QueryProps } from 'react-apollo';
+import {withRouter, RouteComponentProps, Link} from 'react-router-dom';
+import {graphql, QueryProps} from 'react-apollo';
 
 import * as StudentListQueryGql from './StudentListQuery.graphql';
-import { StudentListQuery, StudentSummaryFragment } from '../../types';
+import {StudentListQuery, StudentSummaryFragment} from '../../types';
 import withLoadingHandler from '../../../components/withLoadingHandler';
 
-const StudentRow = ({ student }: { student: StudentSummaryFragment }) => (
+const StudentRow = ({student}: {student: StudentSummaryFragment}) => (
   <tr key={student.id}>
     <td>{student.sName}</td>
     <td className="hidden-sm hidden-xs">{student.attendance}</td>
   </tr>
 );
 
-const StudentsTable = ({ students }: { students: StudentSummaryFragment[] }) => (
+const StudentsTable = ({students}: {students: StudentSummaryFragment[]}) => (
   <table className="striped-table">
     <thead>
       <tr>
@@ -33,7 +33,7 @@ type StudentListPageProps = {
   data: QueryProps & StudentListQuery;
 };
 
-const StudentProfilePage = ({ data: { students } }: StudentListPageProps) => (
+const StudentProfilePage = ({data: {students}}: StudentListPageProps) => (
   // <section className="customCss">
   //   <h2 className="heading">
   //     {/* {students.length}  */}
@@ -44,123 +44,145 @@ const StudentProfilePage = ({ data: { students } }: StudentListPageProps) => (
   //     Add Student
   //     </Link> */}
   // </section>;
-  <section className="plugin-bg-white">
+  <section className="plugin-bg-white p-1">
     <h3 className="bg-heading p-1">
       <i className="fa fa-university stroke-transparent mr-1" aria-hidden="true" /> Admin
       - Student Overview
     </h3>
-    <div className="hflex bg-heading mt-3">
-      <h4>Student Details</h4>
-      <div className="hhflex">
-        <a href="" className="btn btn-primary btn-width bt-radius">
-          Create New Student
-        </a>
-        <a href="" className="btn btn-primary btn-width bt-radius mx-2">
-          Export
-        </a>
-        <a href="" className="btn btn-primary btn-width bt-radius">
-          Save
-        </a>
+    <div className="b-1 m-1">
+      <div className="main-grid p-2">
+        <div className="left-grid">
+          <img src="/public/img/cubes.png" alt="" />
+        </div>
+        <div className="righ-grid">
+          <div className="heading-flex">
+            <h3>Jeremy Andrew Rose</h3>
+            <h5 className="hflex-h5">
+              Contact No: <span>878545625</span>
+            </h5>
+            <h5>
+              Primary Contact No: <span>878545625</span>
+            </h5>
+          </div>
+          <div className="next-div">
+            <span>
+              Admission No: <span>55951426</span>
+            </span>
+            <span>
+              Roll No: <span>2019-521</span>
+            </span>
+            <span>
+              Class: <span>First Year</span>
+            </span>
+            <span>
+              Student Id: <span>951426</span>
+            </span>
+            <span>
+              Department: <span>Computer Science</span>
+            </span>
+            <span>
+              Section: <span>C</span>
+            </span>
+          </div>
+          <div className="buttons">
+            <a href="" className="btn btn-primary">
+              Profile
+            </a>
+            <a href="" className="btn btn-disable">
+              Details
+            </a>
+            <a href="" className="btn btn-disable">
+              Academic
+            </a>
+            <a href="" className="btn btn-disable">
+              Fee
+            </a>
+            <a href="" className="btn btn-disable">
+              Reports
+            </a>
+            <a href="" className="btn btn-disable">
+              ID Card
+            </a>
+            <a href="" className="btn btn-disable">
+              Documents
+            </a>
+            <a href="" className="btn btn-disable">
+              Transport
+            </a>
+          </div>
+        </div>
       </div>
-    </div>
-    <div className="p-1">
-      <div className="dflex j-start">
-        <div className="fwidth pr-1">
-          <label>Department</label>
-          <select className="fwidth">
-            <option value="">Computer Science</option>
-            <option value="">Information Technology</option>
-            <option value="">Electrical</option>
-          </select>
+      <div className="main-details m-1 p-2 b-1">
+        <div className="family-details">
+          <h5>
+            Fathers Name: <input type="text" placeholder="Fathers Full Name" />
+          </h5>
+          <h5>
+            Fathers Contact: <input type="text" placeholder="895865854" />
+          </h5>
+          <h5>
+            Email Address:<input type="text" placeholder="student@father.com" />
+          </h5>
+          <h5>
+            Mothers Name: <input type="text" placeholder="Mothers Full Name" />
+          </h5>
+          <h5>
+            Mothers Contact: <input type="text" placeholder="843545156" />
+          </h5>
+          <h5>
+            Email Address: <input type="text" placeholder="student@mother.com" />
+          </h5>
         </div>
-        <div className="fwidth pr-1">
-          <label>Year</label>
-          <select className="fwidth">
-            <option value="">Year 1</option>
-            <option value="">Year 2</option>
-            <option value="">Year 3</option>
-            <option value="">Year 4</option>
-          </select>
-          {/* <DatePickerComponent /> */}
-          {/* <input type="date" name="" id="" value="12/06/19" /> */}
+        <div className="contact-details">
+          <h5>
+            Address Line 1: <input type="text" placeholder="Address" />
+          </h5>
+          <h5>
+            Address Line 2: <input type="text" placeholder="Address" />
+          </h5>
+          <h5>
+            Town:<input type="text" placeholder="Town" />
+          </h5>
+          <h5>
+            State: <input type="text" placeholder="State" />
+          </h5>
+          <h5>
+            Country: <input type="text" placeholder="Country" />
+          </h5>
+          <h5>
+            Pin Code: <input type="text" placeholder="500008" />
+          </h5>
         </div>
-        <div className="fwidth">
-          <label>Semester</label>
-          <select className="fwidth">
-            <option value="">Div - 1</option>
-            <option value="">Div - 2</option>
-          </select>
-        </div>
-        <div className="fwidth pr-1">
-          <label>Section</label>
-          <select className="fwidth">
-            <option value="">Div - 1</option>
-            <option value="">Div - 2</option>
-          </select>
-        </div>
-        <div className="fwidth pr-1">
-          <label>Gender</label>
-          <select className="fwidth">
-            <option value="">Male</option>
-            <option value="">Female</option>
-          </select>
-        </div>
-        <div className="fwidth pr-1">
-          <label>Student Type</label>
-          <select className="fwidth">
-            <option value="">Scholarship</option>
-            <option value="">Management</option>
-          </select>
+        <div className="id-details">
+          <h5>
+            Adhar No: <input type="text" placeholder="Adhar No" />
+          </h5>
+          <h5>
+            Date Of Birth: <input type="text" placeholder="1/1/2000" />
+          </h5>
+          <h5>
+            Place Of Birth:<input type="text" placeholder="Hyderabad" />
+          </h5>
+          <h5>
+            Religion: <input type="text" placeholder="Religion" />
+          </h5>
+          <h5>
+            Cast: <input type="text" placeholder="Cast" />
+          </h5>
+          <h5>
+            Sub Cast: <input type="text" placeholder="Sub Cast" />
+          </h5>
+          <h5>
+            Blood Group: <input type="text" placeholder="Blood Group" />
+          </h5>
+          <h5>
+            Sex: <input type="text" placeholder="Male/Female" />
+          </h5>
+          <h5>
+            Student Type: <input type="text" placeholder="Student Type" />
+          </h5>
         </div>
       </div>
-      <h3 className="bg-heading mt-3 p-1 mb-0">Computer Science Sem 1</h3>
-      <table className="fwidth">
-        <thead>
-          <th>
-            <input type="checkbox" />
-          </th>
-          <th>Name</th>
-          <th>Student</th>
-          <th>Department</th>
-          <th>Year</th>
-          <th>Section</th>
-          <th>Gender</th>
-          <th>Type</th>
-          <th>Primary</th>
-        </thead>
-        <tbody>
-          <tr>
-            <td>
-              <input type="checkbox" />
-            </td>
-            <td>
-              <a href="">Andrew Russel</a>
-            </td>
-            <td>2019-21</td>
-            <td>Robotic Science</td>
-            <td>Second</td>
-            <td>C</td>
-            <td>Male</td>
-            <td>Scholarship</td>
-            <td>0000-111-222</td>
-          </tr>
-        </tbody>
-        <tr>
-          <td>
-            <input type="checkbox" />
-          </td>
-          <td>
-            <a href="">Jeremy Rose</a>
-          </td>
-          <td>2019-21</td>
-          <td>Electronics Science</td>
-          <td>Third</td>
-          <td>C</td>
-          <td>Male</td>
-          <td>Scholarship</td>
-          <td>0000-111-222</td>
-        </tr>
-      </table>
     </div>
   </section>
 );
