2a37734a8ba5253ae8a4e5747d76d7f8572dcf69
Imran Mohd <imran.mohd@synectiks.com>
2020-04-17 17:30:18 +0530

    title and icon issue fixed

--- src/domain/student/TabPage.tsx
+++ src/domain/student/TabPage.tsx
@@ -4,9 +4,8 @@ import {TabContent, TabPane, Nav, NavItem, NavLink} from 'reactstrap';
 import {FaUserGraduate} from 'react-icons/fa';
 import StudentSubtabs from './SubTabs';
 import {config} from '../../config';
-const LOGGED_IN_USER = new URLSearchParams(location.search).get("signedInUser");
+const LOGGED_IN_USER = new URLSearchParams(location.search).get('signedInUser');
 export default class StudentsTab extends React.Component<any, any> {
-
   constructor(props: any) {
     super(props);
     this.state = {
@@ -21,10 +20,10 @@ export default class StudentsTab extends React.Component<any, any> {
     await this.performLoading();
   }
 
-  async performLoading(){
-    if(!this.state.isLoaded){
-      await  this.setState({
-        isLoaded: true
+  async performLoading() {
+    if (!this.state.isLoaded) {
+      await this.setState({
+        isLoaded: true,
       });
     }
   }
@@ -40,24 +39,25 @@ export default class StudentsTab extends React.Component<any, any> {
       <section className="tab-container">
         <div className="tab-flex p-1">
           <h5>
-            <FaUserGraduate className="m-1 fa-2x m-r-1" />
+            <FaUserGraduate className="m-1 fa-2x" />
             Student
           </h5>
         </div>
         <Nav tabs className="pl-3 pl-3 mb-4 mt-4 boxShadow">
           <NavItem className="cursor-pointer">
-            <NavLink className={`${activeTab === 0 ? 'active' : ''}`} onClick={() => { this.toggleTab(0); }} >
+            <NavLink
+              className={`${activeTab === 0 ? 'active' : ''}`}
+              onClick={() => {
+                this.toggleTab(0);
+              }}
+            >
               Student
             </NavLink>
           </NavItem>
         </Nav>
         <TabContent activeTab={activeTab} className="border-right">
           <TabPane tabId={0}>
-            {
-              isLoaded === true ?
-                <StudentSubtabs user={LOGGED_IN_USER} />
-              : null
-            }
+            {isLoaded === true ? <StudentSubtabs user={LOGGED_IN_USER} /> : null}
           </TabPane>
         </TabContent>
       </section>
